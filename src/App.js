7c9a2139f565130808932d8caf283f5d75eea7b8
dipakpariyar <dipakpriyar499@gmail.com>
2021-06-30 16:57:03 +0545

    mytod

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+  const [doneItems, setDoneItems] = useState([]);
+  const [showDoneList, changeDoneList] = useState(false);
+
+  const eventItems = (event) => {
+    setInputList(event.target.value)
+
+  }
+
+
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    })
+    setInputList('');
+  }
+
+  const doneClickHandler = (idx) => {
+    console.log('done button clicked', idx);
+    const currentValue = items[idx];
+
+    setDoneItems((oldValue) => [...oldValue, currentValue]);
+
+    const currentResult = items.filter((item, index) => idx !== index);
+
+    console.log(
+      'currentvalue ===>', currentValue,
+      'items ====>', items,
+      "currentResult ===>", currentResult
+    );
+
+    setItems(currentResult);
+  }
+
+  const clearAll = () => {
+    console.log("hello");
+    setItems([]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_div">
+
+      <div className="center_div" >
+
+        <input value={inputList} type="text" placeholder="enter text" onChange={eventItems}
+        />
+        <button type="button" onClick={listOfItems}>Add</button>
+        <ol className="main_list">
+          {
+
+            items.map((itemval, index) => {
+              return <li className="list" key={index}>
+                <li> <p>{itemval}</p></li>
+                <button onClick={() => { doneClickHandler(index) }}>Done</button>
+              </li>;
+            })
+          }
+        </ol>
+
+        <ol className="main_list" >
+          <button onClick={() => { changeDoneList(!showDoneList) }} style={{ color: 'white' }}>Done({doneItems.length})</button>
+          {
+            showDoneList ? doneItems.map((itemval, index) => {
+              return <li className="list" key={index} style={{ background: 'red' }}>
+                <li> <p>{itemval}</p></li>
+                <button onClick={() => { doneClickHandler(index) }}>Undo</button>
+              </li>;
+            }) : null
+          }
+        </ol>
+        <div className="btm_div">
+          <button onClick={() => clearAll()}>clrAll</button>
+        </div>
+
+
+      </div>
+
+
     </div>
+
+
   );
 }
+
 
 export default App;
